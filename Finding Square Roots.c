0633a528714eeddb9f11279dc60ba09871d7b2e0
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-06 07:47:25 +0600

    Finding the Rooots  :alien:

---
+++ Finding Square Roots.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main(void) {
+	int x,y,z;
+	scanf("%d",&x);
+	while(x--)
+	{
+	scanf("%d",&y);
+	z=sqrt(y);
+	printf("%d\n",z);
+
+
+	}
+	return 0;
+}
+
+// t=x n=y r=z
