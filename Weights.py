1d49f95f3a8e0ba2596db3e3109830f7cac2a175
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-10 07:33:52 +0600

    Weights :bug ️

--- Weights.py
+++ Weights.py
@@ -2,7 +2,7 @@ for i in range(int(input())):
     (W, X, Y, Z) = map(int, input().split(' '))
     if X==W or Y==W or Z==W:
         print("YES")
-    elif (X+Y) == W or (X+Z) == W or (Y+Z)==w or (X+Y+Z) ==W:
+    elif (X+Y) == W or (X+Z) == W or (Y+Z)==W or (X+Y+Z) ==W:
         print("YES")
     else:
         print("NO")
