823f7b2f5a90be2ee01a68a32b841d41654bc4eb
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-10 07:28:39 +0600

    Weighta :rocket: ️

---
+++ Weights.py
@@ -0,0 +1,8 @@
+for i in range(int(input())):
+    (W, X, Y, Z) = map(int, input().split(' '))
+    if X==W or Y==W or Z==W:
+        print("YES")
+    elif (X+Y) == W or (X+Z) == W or (Y+Z)==w or (X+Y+Z) ==W:
+        print("YES")
+    else:
+        print("NO")
