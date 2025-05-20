8676fd1349ae837f0b282fcd3d82e43571bb1616
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-20 08:30:38 +0600

    Chefland Games  💌

---
+++ Chefland Games.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    r1,r2,r3,r4 = map(int, input().split(' '))
+    
+    if r1 == 1 or r2 == 1 or r3==1 or r4 == 1:
+        print("OUT")
+    else:
+        print('IN')
+        
