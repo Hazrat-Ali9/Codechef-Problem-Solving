73b902869869043955e156445c21ce92ea972b20
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-02 09:46:42 +0600

    Best Of Two  ✈

--- Best of Two.py
+++ Best of Two.py
@@ -1,6 +1,17 @@
-for _ in range(int(input())):
-    a,b=map(int, input().split(' '))
-    if a>=b:
-        print(a)
+T = int(input())
+
+for _ in range(T):
+    rolls = list(map(int, input().split()))
+    alice = sorted(rolls[:3], reverse=True)
+    bob = sorted(rolls[3:], reverse=True)
+
+    alice_score = alice[0] + alice[1]
+    bob_score = bob[0] + bob[1]
+
+    if alice_score > bob_score:
+        print("Alice")
+    elif bob_score > alice_score:
+        print("Bob")
     else:
-        print(b)
+        print("Tie")
+
