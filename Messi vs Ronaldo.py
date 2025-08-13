012d13c251d4f5ac510891736f154c891e524b1d
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-08-13 10:31:06 +0600

    Messi vs Ronaldo 🚅

---
+++ Messi vs Ronaldo.py
@@ -0,0 +1,13 @@
+A, B, X, Y = map(int, input().split())
+
+messi_points = A * 2 + B
+ronaldo_points = X * 2 + Y
+
+
+if messi_points > ronaldo_points:
+    print("Messi")
+elif messi_points < ronaldo_points:
+    print("Ronaldo")
+else:
+    print("Equal")
+
