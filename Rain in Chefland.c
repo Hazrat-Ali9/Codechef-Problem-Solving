e773e87ec902621f739de65a1ee56d4b17eb2eae
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-08-18 12:32:21 +0600

    Rain in Chefland  🛸

---
+++ Rain in Chefland.c
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        if (n < 3) {
+            cout << "LIGHT\n";
+        } else if (n < 7) {
+            cout << "MODERATE\n";
+        } else {
+            cout << "HEAVY\n";
+        }
+    }
+    return 0;
+}
+
