f3994c8e3f15640414a8b67ab672248f0e299bdc
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-08-19 12:02:08 +0600

    Bidding  🛸

---
+++ Bidding.c
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a > b && a > c) {
+            cout << "Alice\n";
+        } 
+        else if (b > a && b > c) {
+            cout << "Bob\n";
+        } 
+        else {
+            cout << "Charlie\n";
+        }
+    }
+    return 0;
+}
