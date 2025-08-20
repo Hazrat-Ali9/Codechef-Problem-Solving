563faf35e6d2df7c95825ca2029f0f9e84e36450
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-08-20 11:37:39 +0600

    Overspeeding Fine 👿

---
+++ Overspeeding Fine.c
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        if (n <= 70) {
+            cout << 0 << "\n";
+        } 
+        else if (n <= 100) {
+            cout << 500 << "\n";
+        } 
+        else {
+            cout << 2000 << "\n";
+        }
+    }
+    return 0;
+}
+
+
