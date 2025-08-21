7a5561e3016d9040cf3e63d45c81bf7b86ef71fd
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-08-21 07:59:29 +0600

    Chess Time 👿

---
+++ Chess Time.c
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int p;
+        cin >> p;
+        cout << p * 3 << "\n";
+    }
+    return 0;
+}
+
+
