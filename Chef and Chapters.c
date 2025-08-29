4a7304ce3fae17992626e1ecbbb5c74bdd2546ce
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-08-29 10:50:15 +0600

    Chef and Chapters 🤪

---
+++ Chef and Chapters.c
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+
+    int t;
+    cin >> t;
+    while (t--) {
+        long long x, y, z;
+        cin >> x >> y >> z;
+        cout << x * y * z << "\n";
+    }
+    return 0;
+}
+
+
