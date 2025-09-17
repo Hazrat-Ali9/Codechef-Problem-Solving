29ee66db7672b0f2b824645a54680151b6f5b021
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-09-17 13:17:58 +0600

    Discount 🤪

---
+++ Discount.c
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cout << "";
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        int x;
+        cout << "";
+        cin >> x;
+        cout << 100 - x << endl;
+    }
+
+    return 0;
+}
+
--- The Cheaper Cab.py
+++ The Cheaper Cab.py
@@ -1,4 +1,5 @@
 t = int(input())
+
 for _ in range(t):
     x, y = map(int, input().split())
     if x > y:
