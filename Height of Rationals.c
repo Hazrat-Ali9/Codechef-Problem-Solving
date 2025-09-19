23811738cce896589eac867acb50760f00f9f6d0
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-09-19 18:04:00 +0600

    Instagram 🤪

--- Height of Rationals.c
+++ Height of Rationals.c
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if (a > b)
+    {
+        cout << a << endl;
+    }
+    else if (a == b)
+    {
+
+        cout << (double)a / b << endl;
+    }
+    else
+    {
+        cout << b << endl;
+    }
+
+    return 0;
+}
 
