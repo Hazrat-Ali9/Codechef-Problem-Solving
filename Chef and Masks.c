800566ebd05c338e407f9d99f929cdeec7399ce9
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-09-28 19:00:26 +0600

    chef and masks  :bulb:

---
+++ Chef and Masks.c
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int x, y;
+        cin >> x >> y;
+        if (100 * x >= 10 * y)
+            cout << "Cloth" << endl;
+        else
+            cout << "Disposable" << endl;
+    }
+    return 0;
+}
+
+
